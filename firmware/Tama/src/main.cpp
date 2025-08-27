ab19763de7316fdca297d831482738f15811a16f
LemmiwinksT800 <randomimaginationdev@gmail.com>
2025-08-27 21:08:54 +0500

    minimum

--- firmware/Tama/src/main.cpp
+++ firmware/Tama/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <pictures.hpp>
+#include <DFPlayerMini_Fast.h>
+#include <SoftwareSerial.h>
 #define CLK 11
 #define OE   9
 #define LAT 10
@@ -9,13 +11,20 @@
 #define C   A2
 #define D   A3
 
+SoftwareSerial Ser(3, 2);
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, true, 64);
+DFPlayerMini_Fast MP3;
 
 void setup() {
   matrix.begin();
+  Serial.begin(9600);
+  Ser.begin(9600);
+  MP3.begin(Ser);
+  MP3.volume(20);
 }
 
 void loop() {
+  
   matrix.setCursor(0, 0);
   matrix.drawRGBBitmap(0,  0, standart, 64, 32);
   matrix.swapBuffers(false);
@@ -37,5 +46,15 @@ void loop() {
   else if (scenary == 2){
       matrix.drawRGBBitmap(0, 0, standart, 64, 32);
       matrix.swapBuffers(false);
+  }
+  if (Serial.available()) {
+    String cmd = Serial.readStringUntil('\n');
+    cmd.trim();
+    if (cmd == "1") {
+      MP3.play(1);
+    }
+    else if (cmd == "2") {
+      MP3.play(2);
+    }
   }
 }
